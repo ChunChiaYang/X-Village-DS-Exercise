773cdf5d01f82d730d21e0a014ad13c409d3854e
ChunChiaYang <Vjames861015@gmail.com>
2018-07-25 14:13:26 +0800

    Complete ex2.py

--- exercise/ex2.py
+++ exercise/ex2.py
@@ -6,8 +6,17 @@ from lib.queue import Queue
 
 def hot_potato(name_list, num):
     # Finish the function
+    q=Queue()
+    for i in name_list:
+        q.enqueue(i)
+    while q.size()>1:
+        for _ in range(num):
+            tmp=q.dequeue()
+            q.enqueue(tmp)
+        q.dequeue()
+    remaining_person=q.dequeue()
 
     return remaining_person
 
-hot_potato(["Susan", "Brad", "Kent", "David"], 6)               # 回傳 "Brad"
-hot_potato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 7)  # 回傳 "Susan"
+print(hot_potato(["Susan", "Brad", "Kent", "David"], 6))              # 回傳 "Brad"
+print(hot_potato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 7))  # 回傳 "Susan"
